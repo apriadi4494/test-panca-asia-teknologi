89c39f1672652d92f3401f6f7eebbdddee30b4b3
Apriadi <apripeni15@gmail.com>
2023-01-26 22:34:45 +0700

    fix(register): added section choose payment method

--- src/components/register/section3.js
+++ src/components/register/section3.js
@@ -1,11 +1,188 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
+import Image from 'next/image';
 import React from 'react';
 
+const payments = [
+  {
+    id: 1,
+    label: 'VIRTUAL ACCOUNT',
+    icon: '/images/payment-icon/virtual-account.svg',
+    images: '/images/payment-icon/virtual-account-list.svg',
+    width: 16,
+    height: 16,
+    widthList: 422.81,
+    heightList: 24,
+  },
+  {
+    id: 2,
+    label: 'BANK TRANSFER',
+    icon: '/images/payment-icon/bank-transfer.svg',
+    images: '/images/payment-icon/bank-transfer-list.svg',
+    width: 16,
+    height: 16,
+    widthList: 434.41,
+    heightList: 24,
+  },
+  {
+    id: 4,
+    label: 'E-WALLET',
+    icon: '/images/payment-icon/e-wallet.svg',
+    images: '/images/payment-icon/e-walet-list.svg',
+    width: 16,
+    height: 16,
+    widthList: 475.72,
+    heightList: 24,
+  },
+  {
+    id: 4,
+    label: 'MODERN STORE',
+    icon: '/images/payment-icon/modern-store.svg',
+    images: '/images/payment-icon/moderen-store-list.svg',
+    width: 16,
+    height: 16,
+    widthList: 436.75,
+    heightList: 24,
+  },
+  {
+    id: 5,
+    label: 'CLICKPAY',
+    icon: '/images/payment-icon/clickpay.svg',
+    images: '/images/payment-icon/clickpay-list.svg',
+    width: 16,
+    height: 16,
+    widthList: 436.75,
+    heightList: 24,
+  },
+  {
+    id: 6,
+    label: 'PAYPAL',
+    icon: '/images/payment-icon/paypal.svg',
+    images: '/images/payment-icon/paypal-list.svg',
+    width: 16,
+    height: 16,
+    widthList: 436.75,
+    heightList: 24,
+  },
+];
+
+function ListPayment(props) {
+  const { label, icon, width = 16, height = 16, images, widthList, heightList } = props;
+  return (
+    <div className="grid grid-cols-2 border border-gray-300 rounded-md">
+      <div className="flex w-full p-4">
+        <Image className="mr-5 ml-1" src={icon} width={width} height={height} />
+        <p>{label}</p>
+      </div>
+      <div className="grid items-center mr-5">
+        <Image className="mr-5 ml-1" src={images} width={widthList} height={heightList} />
+      </div>
+    </div>
+  );
+}
 function Section3() {
   return (
-    <div className="text-register px-10">
-      <div className="flex gap-5 mt-5" />
+    <div className="text-register px-10 mt-20" style={{ height: 680 }}>
+      <div className="grid grid-cols-5 gap-5 mt-5">
+        <div className="grid col-span-3 gap-y-4">
+          <div className="mb-5">
+            <p>METODE PEMBAYARAN</p>
+          </div>
+          {payments.map((payment) => (
+            <ListPayment
+              key={payment.id}
+              label={payment.label}
+              icon={payment.icon}
+              images={payment.images}
+              width={payment.width}
+              height={payment.height}
+              widthList={payment.widthList}
+              heightList={payment.heightList}
+            />
+          ))}
+        </div>
+        <div className="col-span-2 w-full px-5">
+          <div>
+            <p>ORDER SUMMARY</p>
+          </div>
+          <div className="bg-cyan-50 border-2 rounded-md w-full p-3 mt-5" style={{ backgroundColor: '#FBFBFD' }}>
+            <div className="grid grid-cols-2 mb-5">
+              <div>
+                <p className="font-bold" style={{ fontSize: 15 }}>
+                  Blazing Premium
+                </p>
+                <p className="text-gray-400" style={{ fontSize: 13 }}>
+                  1 Tahun
+                </p>
+              </div>
+              <div>
+                <p className="text-right">IDR 2.475.000,-</p>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 mb-5">
+              <div>
+                <p className="font-bold" style={{ fontSize: 15 }}>
+                  50,000 Contacts
+                </p>
+                <p className="text-gray-400" style={{ fontSize: 13 }}>
+                  (Included)
+                </p>
+              </div>
+              <div>
+                <p className="text-right">IDR 0,-</p>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 mb-5">
+              <div>
+                <p className="font-bold" style={{ fontSize: 15 }}>
+                  6 WhatsApp Numbers
+                </p>
+                <p className="text-gray-400" style={{ fontSize: 13 }}>
+                  (Included)
+                </p>
+              </div>
+              <div>
+                <p className="text-right">IDR 0,-</p>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 mt-5 mb-5">
+              <div>
+                <p className="font-bold" style={{ fontSize: 15 }}>
+                  Sub Total
+                </p>
+              </div>
+              <div>
+                <p className="text-right">IDR 2.475.000,-</p>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 mt-5 mb-5">
+              <div>
+                <p className="font-bold text-green-600" style={{ fontSize: 15 }}>
+                  Diskon (60%)
+                </p>
+              </div>
+              <div>
+                <p className="text-green-600 text-right">-IDR 1.485.000,-</p>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 mt-10 mb-5">
+              <div>
+                <p className="font-bold" style={{ fontSize: 15 }}>
+                  Grand Total
+                </p>
+              </div>
+              <div>
+                <p className="text-right">IDR 990.000,-</p>
+              </div>
+            </div>
+          </div>
+          <div className="mt-5">
+            <p style={{ fontSize: 14 }}>
+              Punya Kode Kupon? <span className="text-red-500 cursor-pointer duration-700 hover:text-red-800">Klik di sini</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -32,21 +32,28 @@ function Register() {
         {section === 'section2' && <Section2 setSection={setSection} />}
         {section === 'section3' && <Section3 setSection={setSection} />}
 
-        <div className="mt-5 px-5">
-          <div className="grid grid-cols-2 w-full">
-            <button onClick={() => setSection('section1')} type="button" className="flex w-full justify-end gap-5 py-3 rounded-l-lg bg-gray-50 border duration-500 hover:bg-gray-200">
-              <Image src="/images/arrow-left-gray.svg" alt="arrow-right" width={14} height={16} />
-              <p className="mr-5 font-bold">SEBELUMNYA</p>
-            </button>
-            <button onClick={() => setSection('section3')} type="button" className="flex w-full gap-5 py-3 rounded-r-lg bg-blue-500 border duration-500 hover:bg-blue-700">
-              <p className="ml-5 text-white font-bold">SELANJUTNYA</p>
-              <Image className="mr-5" src="/images/arrow-right.svg" alt="arrow-left" width={14} height={16} />
-            </button>
+        {section !== 'section1' && (
+          <div>
+            <div className="bottom-0">
+              {' '}
+              <div className="mt-5 px-5">
+                <div className="grid grid-cols-2 w-full">
+                  <button onClick={() => setSection('section1')} type="button" className="flex w-full justify-end gap-5 py-3 rounded-l-lg bg-gray-50 border duration-500 hover:bg-gray-200">
+                    <Image src="/images/arrow-left-gray.svg" alt="arrow-right" width={14} height={16} />
+                    <p className="mr-5 font-bold">SEBELUMNYA</p>
+                  </button>
+                  <button onClick={() => setSection('section3')} type="button" className="flex w-full gap-5 py-3 rounded-r-lg bg-blue-500 border duration-500 hover:bg-blue-700">
+                    <p className="ml-5 text-white font-bold">SELANJUTNYA</p>
+                    <Image className="mr-5" src="/images/arrow-right.svg" alt="arrow-left" width={14} height={16} />
+                  </button>
+                </div>
+              </div>
+              <div className="w-full mt-1 px-5">
+                <p className="text-blue-700 text-xs text-right">Skip for now</p>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="w-full mt-1 px-5">
-          <p className="text-blue-700 text-xs text-right">Skip for now</p>
-        </div>
+        )}
       </div>
     </div>
   );
