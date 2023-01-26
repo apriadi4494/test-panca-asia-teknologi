b8e6ecf015800e09f38af067e575a335d601269f
Apriadi <apripeni15@gmail.com>
2023-01-26 12:58:08 +0700

    package: added prime react library

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable import/no-unresolved */
+import '@/styles/globals.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
