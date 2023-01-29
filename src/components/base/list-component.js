2243df1ec6e9ef92af07623dfbdbe241a479f341
Apriadi <apripeni15@gmail.com>
2023-01-29 08:43:55 +0700

    fix(list-component): fix linter

--- src/components/base/list-component.js
+++ src/components/base/list-component.js
@@ -4,7 +4,9 @@ function ListComponent({ text, icon, fontSize = 17, width = 20, height = 20 }) {
   return (
     <div className="flex gap-5 mb-1">
       <Image src={icon} width={width} height={height} />
-      <p className="font-normal opacity-80 text-popins" style={{ fontSize }}>{text}</p>
+      <p className="font-normal opacity-80 text-popins" style={{ fontSize }}>
+        {text}
+      </p>
     </div>
   );
 }
